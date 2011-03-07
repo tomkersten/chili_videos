03bb8192357faeab627b28f12fccfee516982f41
Tom Kersten <tom@whitespur.com>
2011-03-07 17:38:03 -0600

    Added Config#update

--- app/models/chili_video_plugin/config.rb
+++ app/models/chili_video_plugin/config.rb
@@ -14,9 +14,27 @@ module ChiliVideoPlugin
       plugin_settings[WORKFLOW_KEYNAME]
     end
 
+    # Accepts anything which responds to '[]' and '.has_key?' # (Hash-like objects)
+    #
+    # Requires & utilizes the following keys
+    #   - :api_key
+    #   - :workflow
+    def update(options)
+      unless options.has_key?(:api_key) && options.has_key?(:workflow)
+        raise ArgumentError, "Missing key(s) in: #{options.inspect}"
+      end
+      Setting[PLUGIN_KEYNAME] = formatted_hash(options[:api_key], options[:workflow])
+      true
+    end
+
     private
       def plugin_settings
         Setting[PLUGIN_KEYNAME]
+      end
+
+      def formatted_hash(api_key, workflow)
+        HashWithIndifferentAccess.new({API_KEY_KEYNAME => api_key,
+                                      WORKFLOW_KEYNAME => workflow})
       end
   end
 end
--- test/unit/chili_video_plugin/config_test.rb
+++ test/unit/chili_video_plugin/config_test.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
+include ChiliVideoPlugin
+
 class ConfigTest < ActiveSupport::TestCase
   setup do
     Setting["plugin_chili_videos"] = HashWithIndifferentAccess.new({:transloadit_api_key => 'api_key', :transloadit_workflow => 'workflow_name'})
@@ -14,6 +16,34 @@ class ConfigTest < ActiveSupport::TestCase
     context '.workflow' do
       should "returns the value of the plugin's 'transloadit_workflow' setting" do
         assert_equal 'workflow_name', ChiliVideoPlugin::Config.workflow
+      end
+    end
+
+    context '.update' do
+      context 'when all required parameters are supplied' do
+        setup do
+          @new_values = {:api_key => 'updated-api', :workflow => 'workflow-update'}
+        end
+
+        should "the value of the plugin's 'api_key' setting is updated" do
+          assert_equal 'api_key', ChiliVideoPlugin::Config.api_key
+          ChiliVideoPlugin::Config.update(@new_values)
+          assert_equal @new_values[:api_key], ChiliVideoPlugin::Config.api_key
+        end
+
+        should "the value of the plugin's 'workflow-update'' setting is updated" do
+          assert_equal 'workflow_name', ChiliVideoPlugin::Config.workflow
+          ChiliVideoPlugin::Config.update(@new_values)
+          assert_equal @new_values[:workflow], ChiliVideoPlugin::Config.workflow
+        end
+      end
+
+      context 'when a required parameter is missing' do
+        should "an ArgumentError is raised" do
+          assert_raise(ArgumentError) {
+            ChiliVideoPlugin::Config.update(:api_key => 'api_key')
+          }
+        end
       end
     end
 end
