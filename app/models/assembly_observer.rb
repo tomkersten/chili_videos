f37150c52e42e7abb826350c4f946ea6fe4da005
Roy Kolak and Tom Kersten <devs+roy+tom@gnresound.com>
2011-03-14 11:35:50 -0500

    Set AssemblyObserver to always do async processing

--- app/models/assembly_observer.rb
+++ app/models/assembly_observer.rb
@@ -1,10 +1,6 @@
 class AssemblyObserver < ActiveRecord::Observer
   def after_create(assembly)
-    Rails.logger.info "Processing assembly #{assembly.assembly_url} (id ##{assembly.id})"
-    AssemblyProcessor.process(assembly)
-    Rails.logger.info "Finished processing assembly #{assembly.assembly_url} (id ##{assembly.id})"
-  rescue ChiliVideoPlugin::Error::IncompleteAssembly
-    Rails.logger.info "Assembly #{assembly.assembly_url} (id ##{assembly.id}) is not completed yet. Requeing for processing..."
+    Rails.logger.info "Queing up assembly #{assembly.assembly_url} (id ##{assembly.id})..."
     AssemblyProcessor.delay.process(assembly)
   end
 end
