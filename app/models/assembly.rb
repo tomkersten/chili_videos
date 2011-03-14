ee42aaeac3e416b9a2d777719739b6a335be5b35
Tom Kersten <tom@whitespur.com>
2011-03-13 19:10:30 -0500

    Slight refactor of Assembly
    * Pushed 'raw_assembly' temp variable into method
    * Pushed 'assembly completed' logic into a .completed? method

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -4,17 +4,36 @@ end
 class Assembly < ActiveRecord::Base
   unloadable
 
+  ASSEMBLY_STATUS_KEY     = "ok"
+  ASSEMBLY_COMPLETE_VALUE = "ASSEMBLY_COMPLETED"
+
   attr_accessible :project_id, :ssembly_id, :assembly_url, :user_id
 
   def encodings
-    raw_assembly = Retriever.get(assembly_url)
-    raise ChiliVideoPlugin::Error::IncompleteAssembly unless raw_assembly["ok"] == "ASSEMBLY_COMPLETED"
+    raise ChiliVideoPlugin::Error::IncompleteAssembly unless completed?
 
     raw_assembly["results"]["encode"].map do |raw_encoding|
       Encoding.new(raw_encoding)
     end
   end
 
+  # Retrieves and caches the content of .assembly_url. If you pass
+  # in :reload as an argument, it will visit the URL again and replace
+  # the existing values.
+  #
+  # reload_or_not - determines whether the method call should use a cached
+  #                 version of the content or not. :reload is the only value
+  #                 which will have an impact on the method call.
+  #
+  # Returns the HTTP response as a Hash
+  def raw_assembly(symbol = nil)
+    return @raw_assembly if @raw_assembly && symbol != :reload
+    @raw_assembly = Retriever.get(assembly_url)
+  end
+
+  def completed?
+    raw_assembly(:reload)[ASSEMBLY_STATUS_KEY] == ASSEMBLY_COMPLETE_VALUE
+  end
 
   class << self
     class ::Retriever
