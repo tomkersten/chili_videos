b79df7f812ccfc1b897d259a931c17f9b1850af6
Tom Kersten <tom@whitespur.com>
2011-03-31 14:36:05 -0500

    Added (hacky) asset-copying to rails/init.rb

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,9 @@
 require File.dirname(__FILE__) + "/../init"
+
+source_dir = "#{File.dirname(__FILE__)}/../assets"
+destination_dir = "#{RAILS_ROOT}/public/plugin_assets/chili_videos"
+
+# HACK: Copies the files from plugindir/assets to the appropriate place in
+# the rails application
+puts "Copying ChiliVideo asset files to #{destination_dir}"
+system("cp -r #{source_dir} #{destination_dir}")
