3b8f40210b2921f9be2a7df5693943da01afd5c5
Tom Kersten <tom@whitespur.com>
2011-04-02 01:30:14 -0500

    Switched (hacky) asset loader to symlink (vs copy)

--- rails/init.rb
+++ rails/init.rb
@@ -3,7 +3,9 @@ require File.dirname(__FILE__) + "/../init"
 source_dir = "#{File.dirname(__FILE__)}/../assets"
 destination_dir = "#{RAILS_ROOT}/public/plugin_assets/chili_videos"
 
-# HACK: Copies the files from plugindir/assets to the appropriate place in
+# HACK: Symlinks the files from plugindir/assets to the appropriate place in
 # the rails application
-puts "Copying ChiliVideo asset files to #{destination_dir}"
-system("cp -r #{source_dir} #{destination_dir}")
+system("unlink #{destination_dir}") if File.exists?(destination_dir)
+
+puts "Symlinking ChiliVideo asset files to #{destination_dir}"
+system("ln -s #{source_dir} #{destination_dir}")
